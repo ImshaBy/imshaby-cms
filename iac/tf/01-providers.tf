29b61695ba451182a3a7d9781d2cd913ca36eb3f
Andrei Misan <andrei.misan@gmail.com>
2023-05-30 13:16:18 +0300

    rename files

--- iac/tf/01-providers.tf
+++ iac/tf/01-providers.tf
@@ -6,13 +6,13 @@ terraform {
   }
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
-    bucket     = var.tr_backend_bucket_name
+    bucket     = "tf-bucket-imshaby"
     region     = "ru-central1"
-    key        = var.tf_backend_key
-    access_key = var.access_key
-    secret_key = var.secret_key
-    dynamodb_endpoint =  var.dynamodb_endpoint
-    dynamodb_table    = var.dynamodb_table
+    key        = "imshaby-cms/production/terraform.tfstate"
+    access_key = ${ACCESS_KEY_ID}
+    secret_key = ${ACCESS_SECRET_KEY}
+    dynamodb_endpoint =  ${DYNAMODB_ENDPOINT}
+    dynamodb_table    = "state-lock-table"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
