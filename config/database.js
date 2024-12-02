6ba8dd3739c972f166ddc504bab17078bfc8604f
NikDark <nikefremov.00.008@hotmail.com>
2024-12-02 18:56:01 +0300

    fix: add connection.pool database config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
+    acquireConnectionTimeout: 5000,
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
@@ -9,5 +10,14 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD', 'admin'),
       ssl: env.bool('DATABASE_SSL', false),
     },
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 8000,
+      acquireTimeoutMillis: 8000,
+      idleTimeoutMillis: 8000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    }
   },
 });
